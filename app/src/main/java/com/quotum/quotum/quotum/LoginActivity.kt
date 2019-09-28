21867ad6bed62cf8e2125516900a3f42e8bf6faa
Kartik Setia <kartiksetia@gmail.com>
2019-09-28 20:21:17 +0530

    changes

--- app/src/main/java/com/quotum/quotum/quotum/LoginActivity.kt
+++ app/src/main/java/com/quotum/quotum/quotum/LoginActivity.kt
@@ -1,5 +1,9 @@
 package com.quotum.quotum.quotum
 
+import android.app.Activity
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
 
@@ -9,6 +13,7 @@ import android.support.v7.app.AppCompatActivity
 
 import android.view.View
 import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
 
 import android.widget.*
 
@@ -24,6 +29,7 @@ class LoginActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_login)
         setVideoBackground()
+        hideSystemUI()
 
 
         val bottomSheet = findViewById<LinearLayout>(R.id.bottom_sheet)
@@ -51,6 +57,7 @@ class LoginActivity : AppCompatActivity() {
             val view = layoutInflater.inflate(R.layout.login_signup_bottom_sheet, null)
             val dialog = BottomSheetDialog(this)
             dialog.setContentView(view)
+            dialog.getWindow().setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
             dialog.show()
         }
 
@@ -61,5 +68,16 @@ class LoginActivity : AppCompatActivity() {
         val path = "android.resource://" + packageName + "/" + R.raw.beach
         videoView?.setVideoURI(Uri.parse(path))
         videoView.start()
+    }
+
+    private fun hideSystemUI() {
+        val decorView = window.decorView
+        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 }
--- app/src/main/java/com/quotum/quotum/quotum/SplashActivity.kt
+++ app/src/main/java/com/quotum/quotum/quotum/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.quotum.quotum.quotum
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import java.util.*
 import kotlin.concurrent.schedule
 
@@ -11,10 +12,22 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        hideSystemUI()
         val intent = Intent(this, LoginActivity::class.java)
         Timer("SettingUp", false).schedule(2000) {
             startActivity(intent)
             finish()
         }
+    }
+
+    private fun hideSystemUI() {
+        val decorView = window.decorView
+        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 }
