85732ee576ffdb74c5d5487e4b2795a800dc9783
Kartik Setia <kartiksetia@gmail.com>
2020-05-18 21:07:51 +0530

    Signup

--- app/src/main/java/com/quotum/quotum/quotum/LoginActivity.kt
+++ app/src/main/java/com/quotum/quotum/quotum/LoginActivity.kt
@@ -1,23 +1,20 @@
 package com.quotum.quotum.quotum
 
-import android.app.Activity
-import android.content.Context
+import android.annotation.TargetApi
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-
 import android.support.design.widget.BottomSheetBehavior
 import android.support.design.widget.BottomSheetDialog
 import android.support.v7.app.AppCompatActivity
-
 import android.view.View
+import android.view.ViewGroup
 import android.view.WindowManager
-import android.view.inputmethod.InputMethodManager
-
 import android.widget.*
 
-import com.quotum.quotum.quotum.R
 
 class LoginActivity : AppCompatActivity() {
 
@@ -30,7 +27,6 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         setVideoBackground()
         hideSystemUI()
-
 
         val bottomSheet = findViewById<LinearLayout>(R.id.bottom_sheet)
         val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
@@ -52,13 +48,41 @@ class LoginActivity : AppCompatActivity() {
         })
 
         val textViewLogin = findViewById(R.id.text_view_login) as TextView
-
+        val view = layoutInflater.inflate(R.layout.login_signup_bottom_sheet, null)
+        val dialog = BottomSheetDialog(this)
         textViewLogin.setOnClickListener {
-            val view = layoutInflater.inflate(R.layout.login_signup_bottom_sheet, null)
-            val dialog = BottomSheetDialog(this)
+            if (view.getParent() != null) {
+                (view.getParent() as ViewGroup).removeView(view)
+            }
             dialog.setContentView(view)
             dialog.getWindow().setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
             dialog.show()
+
+        }
+
+        val relativeLayoutLogin = view.findViewById(R.id.rl_login) as RelativeLayout
+        val relativeLayoutSignup = view.findViewById(R.id.rl_signup) as RelativeLayout
+        val buttonLoginSwitch = view.findViewById(R.id.login_switch) as Button
+        val buttonSignupSwitch = view.findViewById(R.id.signup_switch) as Button
+        val buttonLogin = view.findViewById(R.id.loginButton) as Button
+        relativeLayoutSignup.visibility = View.GONE
+        buttonLoginSwitch.setOnClickListener {
+            buttonLoginSwitch.setBackgroundResource(R.drawable.login_background)
+            buttonSignupSwitch.setBackgroundResource(R.drawable.login_btton_unpressed_background)
+            relativeLayoutLogin.visibility = View.VISIBLE
+            relativeLayoutSignup.visibility = View.GONE
+        }
+
+        buttonSignupSwitch.setOnClickListener {
+            buttonLoginSwitch.setBackgroundResource(R.drawable.login_btton_unpressed_background)
+            buttonSignupSwitch.setBackgroundResource(R.drawable.login_background)
+            relativeLayoutLogin.visibility = View.GONE
+            relativeLayoutSignup.visibility = View.VISIBLE
+        }
+
+        buttonLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java);
+            startActivity(intent);
         }
 
     }
@@ -70,6 +94,8 @@ class LoginActivity : AppCompatActivity() {
         videoView.start()
     }
 
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     private fun hideSystemUI() {
         val decorView = window.decorView
         decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
