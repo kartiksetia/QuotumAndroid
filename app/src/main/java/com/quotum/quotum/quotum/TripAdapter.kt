d78e567114d0d8ba605eb7647ad6b95a2dba0685
Kartik Setia <kartiksetia@gmail.com>
2020-11-09 03:01:25 +0530

    api

--- app/src/main/java/com/quotum/quotum/quotum/TripAdapter.kt
+++ app/src/main/java/com/quotum/quotum/quotum/TripAdapter.kt
@@ -3,9 +3,11 @@ package com.quotum.quotum.quotum
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.quotum.quotum.quotum.models.GetTripLocationResponseModel
 
- class TripAdapter : RecyclerView.Adapter<TripAdapter.TripViewHolder>() {
+class TripAdapter(var data : GetTripLocationResponseModel) : RecyclerView.Adapter<TripAdapter.TripViewHolder>()  {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TripViewHolder {
@@ -13,10 +15,20 @@ import androidx.recyclerview.widget.RecyclerView
         return TripViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: TripViewHolder, position: Int) {}
-    override fun getItemCount(): Int {
-        return 8
+    override fun onBindViewHolder(holder: TripViewHolder, position: Int) {
+        holder.textViewFrom.text = data.getResult()?.get(position)?.getSource()
+        holder.textViewDestination.text = data.getResult()?.get(position)?.getDestination()
+        holder.textViewCost.text = data.getResult()?.get(position)?.getBudget().toString()
     }
 
-    class TripViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    override fun getItemCount(): Int {
+        return data.getResult()?.size!!
+    }
+
+    class TripViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val textViewFrom = itemView.findViewById(R.id.text_view_from) as TextView
+        val textViewDestination = itemView.findViewById(R.id.text_view_destination) as TextView
+        val textViewCost = itemView.findViewById(R.id.text_view_cost) as TextView
+
+    }
 }
