24b671b3b38f51f70138ba2155cfde0a5088702c
Kartik Setia <kartiksetia@gmail.com>
2020-11-29 21:40:33 +0530

    set data to textviews trip details

--- app/src/main/java/com/quotum/quotum/quotum/TripAdapter.kt
+++ app/src/main/java/com/quotum/quotum/quotum/TripAdapter.kt
@@ -19,6 +19,11 @@ class TripAdapter(var data : GetTripLocationResponseModel) : RecyclerView.Adapte
         holder.textViewFrom.text = data.getResult()?.get(position)?.getSource()
         holder.textViewDestination.text = data.getResult()?.get(position)?.getDestination()
         holder.textViewCost.text = data.getResult()?.get(position)?.getBudget().toString()
+        holder.textViewVehicle.text = data.getResult()?.get(position)?.getModeOfTransport().toString()
+        holder.textViewDate.text = data.getResult()?.get(position)?.getStartDate().toString()
+        holder.textViewTime.text = data.getResult()?.get(position)?.getDays().toString() + "-days"
+
+
     }
 
     override fun getItemCount(): Int {
@@ -29,6 +34,9 @@ class TripAdapter(var data : GetTripLocationResponseModel) : RecyclerView.Adapte
         val textViewFrom = itemView.findViewById(R.id.text_view_from) as TextView
         val textViewDestination = itemView.findViewById(R.id.text_view_destination) as TextView
         val textViewCost = itemView.findViewById(R.id.text_view_cost) as TextView
+        val textViewVehicle = itemView.findViewById(R.id.textview_vehicle) as TextView
+        val textViewDate = itemView.findViewById(R.id.textview_date) as TextView
+        val textViewTime = itemView.findViewById(R.id.textview_time) as TextView
 
     }
 }
